4120434e4d6b419195a4e4c38107313c7f010be1
alawa2020 <huder13168@gmail.com>
2021-12-16 22:54:19 -0500

    🧶 login componet prototype

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
+import { useForm } from '../hooks/useForm';
 
 const LoginScreen = () => {
+  const { setUser } = useContext(UserContext);
+
+  const [formValues, formValuesChange, resetForm] = useForm({
+    name: '',
+    age: '',
+  });
+
+  const { name, age } = formValues;
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setUser({
+      name,
+      age,
+    });
+    resetForm();
   };
   return (
     <div>
@@ -12,11 +28,17 @@ const LoginScreen = () => {
           className="form-control mb-3"
           type="text"
           placeholder="su nombre..."
+          name="name"
+          value={name}
+          onChange={formValuesChange}
         />
         <input
           className="form-control mb-3"
           type="number"
           placeholder="su edad..."
+          name="age"
+          value={age}
+          onChange={formValuesChange}
         />
 
         <input className="btn btn-primary" type="submit" value="Login" />
---
+++ src/hooks/useForm.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export const useForm = (initialState) => {
+  const [formValues, setFormValues] = useState(initialState);
+
+  const formValuesChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const resetForm = () => {
+    setFormValues(initialState);
+  };
+
+  return [formValues, formValuesChange, resetForm];
+};
